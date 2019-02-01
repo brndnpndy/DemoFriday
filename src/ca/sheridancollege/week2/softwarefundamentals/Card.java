ceac70e55c4d7b84656d2d67fae686076fa0a349
brandon <30944372+brndnpndy@users.noreply.github.com>
2019-02-01 13:05:51 -0500

    Update Card.java

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+// todays date
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
